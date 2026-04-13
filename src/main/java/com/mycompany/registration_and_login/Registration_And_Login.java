0faea92815e142806d364e8ac58d72e8c1d8a311
Student <Student@LabVM2047644.ju4k3udt5hzunlolye3o2piqrc.jnbx.internal.cloudapp.net>
2026-04-13 14:03:57 +0200

    Initial commit_created Registration_And_Login class with fields

---
+++ src/main/java/com/mycompany/registration_and_login/Registration_And_Login.java
@@ -0,0 +1,149 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.registration_and_login;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Student
+ */
+public class Registration_And_Login {
+     String username;
+     String password;
+     String phoneNumber;
+    
+    //====================================
+    // checkUserName()
+    //====================================
+    
+    public boolean checkUserName(){
+        return username.contains("_") && username.length()<=5;
+    }
+    //====================================
+    // checkPasswordComplexity()
+    //====================================
+    
+    public boolean checkPasswordComplexity(){
+        boolean hasUpperCase = false;
+        boolean hasNumber = false;
+        boolean hasSpecialCharacter = false;
+        
+         for(int i=0; i< password.length(); i++){
+            char ch=password.charAt(i);
+            
+            if(Character.isUpperCase(ch)){
+                hasUpperCase = true;
+            }else if(Character.isDigit(ch)){
+                hasNumber = true;
+            }else if(!Character.isLetterOrDigit(ch)){
+                hasSpecialCharacter = true;
+            }
+    }
+         return password.length()>= 8 && hasUpperCase && hasNumber && hasSpecialCharacter;
+    }
+    
+    //=======================================================
+    // checkCellPhoneNumber()
+    //=======================================================
+    
+    public boolean checkCellPhoneNumber(){
+        return phoneNumber.startsWith("+27") && phoneNumber.length() == 12;
+    }
+    
+    //=======================================================
+    // registerUser()
+    //=======================================================
+    
+    public String registerUser(){
+        if(!checkUserName()){
+            return "Username is not correctly formatted; Please ensure that your username contains an underscore and is not more than five characters in length.";
+        }
+        if(!checkPasswordComplexity()){
+            return "Password is not correctly formatted; please ensure that the password contains at least eight characters, a capital letter, a number, and a special character.";
+        }
+        if (!checkCellPhoneNumber()){
+            return "Cellphone number is incorrectly formatted or does not contain the international code.";
+        }
+        return "User has been registered successfully.";
+    }
+    
+    //=======================================================
+    // loginUser()
+    //=======================================================
+    
+    public boolean loginUser(String inputUsername, String inputPassword){
+        return inputUsername.equals(username) && inputPassword.equals(password);
+    }
+    
+    //========================================================
+    // returnLoginStatus()
+    //========================================================
+    
+    public String returnLoginStatus(String inputUsername, String inputPassword, String firstName, String lastName){
+        if(inputUsername.equals(username) && inputPassword.equals(password)){
+            return "Welcome " + firstName + " " + lastName +"  " + "it is great to see you.";
+        }
+        else if(inputUsername.equals(username) && !inputPassword.equals(password)){
+            return "Incorrect Password. Please try again.";
+        }
+        else if(!inputUsername.equals(username) && inputPassword.equals(password)){
+            return "Incorrect username. Please try again.";
+        }else{
+            return "Username and password are incorrect. Please try again.";
+        }
+    }
+    //===========================================================
+    // MAIN METHOD
+    //===========================================================
+
+    public static void main(String[] args) {
+         try (Scanner input = new Scanner(System.in)) {
+             Registration_And_Login user = new Registration_And_Login();
+             
+             //========================================================
+             //REGISTRATION
+             //========================================================
+             
+             System.out.println("Enter your username:");
+             user.username = input.nextLine();
+             
+             System.out.println("Enter your password:");
+             user.password = input.nextLine();
+             
+             System.out.println("Enter your phone number:");
+             user.phoneNumber = input.nextLine();
+             
+             System.out.println(user.registerUser());
+             
+             //=======================================================
+             //LOGIN
+             //=======================================================
+             
+             System.out.println("\nEnter your username:");
+             String loginUsername = input.nextLine();
+             
+             System.out.println("Enter your password:");
+             String loginPassword = input.nextLine();
+             
+             System.out.println("Enter your first name:");
+             String firstName = input.nextLine();
+             
+             System.out.println("Enter your last name:");
+             String lastName = input.nextLine();
+             
+             System.out.println(user.returnLoginStatus(loginUsername, loginPassword, firstName, lastName));
+         }
+    }
+    
+}
+
+//=========================================================================
+// REFERENCE LIST (HAVARD STYLE):
+//=========================================================================
+// Oracle (2024) Java Tutorials. Available at: https://docs.oracle.com/javase/tutorial/
+//
+// W3Schools (2024) Java Tutorials. Available at: https://www.w3schools.com/java/
+
